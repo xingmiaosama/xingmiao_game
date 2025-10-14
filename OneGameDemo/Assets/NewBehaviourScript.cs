42328d6cdb797892f19f097f5333225720ce62fc
xingmiaosama <2068148681@qq.com>
2025-10-14 18:13:50 +0800

    test push

---
+++ OneGameDemo/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float MoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
