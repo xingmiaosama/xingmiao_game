c7be897b05678b81f4c4b49125fdbbf5ca1cc2cc
xingmiaosama <2068148681@qq.com>
2025-10-26 10:48:38 +0800

    Perfect scripts for moving parts

--- OneGameDemo/Assets/config/PlayerData.cs
+++ OneGameDemo/Assets/config/PlayerData.cs
@@ -7,7 +7,9 @@ public class PlayerData : MonoBehaviour
     [Header("移动参数")]
     public float moveSpeed = 5.0f;
     public float jumpForce = 10.0f;
+    public bool canDoubleJump = false;
+
     [Header("物理参数")]
     public float gravityScale = 3.0f;
-
+    public float momentum = 0.1f;
 }
--- OneGameDemo/Assets/script/GameMainProgram.cs
+++ OneGameDemo/Assets/script/GameMainProgram.cs
@@ -7,6 +7,7 @@ public class GameMainProgram : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float horizontalInput;
+    private bool isTouchingGround;
     [SerializeField] private PlayerData playerData;
     // Start is called before the first frame update
 
@@ -23,7 +24,7 @@ public class GameMainProgram : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             rb.velocity = new Vector2(rb.velocity.x,playerData.jumpForce);   
         }
@@ -31,6 +32,35 @@ public class GameMainProgram : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * playerData.moveSpeed,rb.velocity.y);
+        if (isTouchingGround)
+        {
+            rb.velocity = new Vector2(horizontalInput * playerData.moveSpeed * playerData.momentum, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(horizontalInput * playerData.moveSpeed, rb.velocity.y);
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        ContactPoint2D contact = other.contacts[0];
+        if (contact.normal.x > 0.5f || contact.normal.x < -0.5f)
+        {
+            isTouchingGround = true;
+        }
+        else
+        {
+            isTouchingGround = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            
+        }
     }
 }
