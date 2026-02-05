24033d58cbfd17ab1560c740d9e8e7bd78a22315
xingmiaosama <2068148681@qq.com>
2026-02-05 13:32:50 +0800

    UI Update

--- RPG/Assets/Scripts/PlayerHealth.cs
+++ RPG/Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth = 3;
     public int maxHealth = 3;
+    public TMP_Text healthText;
+    public Animator healthTextAnimator;
 
+    private void Start()
+    {
+        healthText.text = "HP:" + currentHealth + "/" + maxHealth;
+    }
 
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
+        healthTextAnimator.Play("TextUpdate");
+        healthText.text = "HP:" + currentHealth + "/" + maxHealth;
+
         if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
